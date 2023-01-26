bc274e365f8584d3ced396d822e4ae7f4ef62e9c
OVOMARK <mcncstatefan@icloud.com>
2023-01-26 12:16:43 -0500

    updated html generation and brought in bootstrap

---
+++ professional-portfolio/src/components/Main.js
@@ -0,0 +1,11 @@
+import Nav from './Nav';
+import Contact from './Contact';
+
+export default (function Main () {
+    return(
+        <div>
+            <Nav/>,
+            <Contact/>,
+        </div>
+    )
+})
