2be91ddd34d4a146ab370779475d2336f68adc07
OVOMARK <mcncstatefan@icloud.com>
2023-01-26 13:52:36 -0500

    added more react components and images

---
+++ professional-portfolio/src/components/Jumbotron.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+export default (function Jumbotron () {
+    return(
+        <header className="jumbotron">
+            <div className="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div className="d-flex justify-content-center">
+                    <div className="text-center">
+                        <h1 className="mx-auto my-0 text-uppercase">Marcus Carlough</h1>
+                        <h2 className="text-white-50 mx-auto mt-2 mb-5">Junior Full Stack Developer</h2>
+                        <a className="btn btn-primary" href="#about">Get Started</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+})
--- professional-portfolio/src/components/Main.js
+++ professional-portfolio/src/components/Main.js
@@ -1,11 +1,28 @@
 import Nav from './Nav';
 import Contact from './Contact';
+import Projects from './Projects';
+import About from './About';
+import Jumbotron from './Jumbotron';
 
 export default (function Main () {
     return(
-        <div>
-            <Nav/>,
-            <Contact/>,
+        <div className='port-main'>
+            <div>
+                <Nav/>
+            </div>
+            <div>
+                <Jumbotron/>
+            </div>
+            <div>
+                <About/>
+            </div>
+            <div>
+                <Projects/>
+            </div>
+            <div>
+                <Contact/>
+            </div>
+            
         </div>
     )
 })
